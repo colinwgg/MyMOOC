40ad7b9ef8cdb3ff823da7544cf5f081cfed9cb5
Colin Wang <2802235800@qq.com>
2024-08-08 21:30:13 +0800

    学习计划和进度-init

---
+++ tj-learning/src/main/java/com/tianji/learning/controller/LearningRecordController.java
@@ -0,0 +1,24 @@
+package com.tianji.learning.controller;
+
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 学习记录表 前端控制器
+ * </p>
+ *
+ * @author colinwang
+ * @since 2024-08-08
+ */
+@Api(tags = "学习记录相关接口")
+@Slf4j
+@RestController
+@RequestMapping("/learning-records")
+public class LearningRecordController {
+
+}
---
+++ tj-learning/src/main/java/com/tianji/learning/service/ILearningRecordService.java
@@ -0,0 +1,16 @@
+package com.tianji.learning.service;
+
+import com.tianji.learning.domain.po.LearningRecord;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 学习记录表 服务类
+ * </p>
+ *
+ * @author colinwang
+ * @since 2024-08-08
+ */
+public interface ILearningRecordService extends IService<LearningRecord> {
+
+}
