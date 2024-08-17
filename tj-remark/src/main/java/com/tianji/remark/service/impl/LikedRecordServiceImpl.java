b9357641d0826aac028afa2771f4054a0deeaed5
Colin Wang <2802235800@qq.com>
2024-08-17 22:53:40 +0800

    点赞系统-点赞或取消点赞

--- tj-remark/src/main/java/com/tianji/remark/service/impl/LikedRecordServiceImpl.java
+++ tj-remark/src/main/java/com/tianji/remark/service/impl/LikedRecordServiceImpl.java
@@ -1,11 +1,20 @@
 package com.tianji.remark.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.tianji.api.dto.remark.LikedTimesDTO;
+import com.tianji.common.autoconfigure.mq.RabbitMqHelper;
+import com.tianji.common.utils.StringUtils;
+import com.tianji.common.utils.UserContext;
 import com.tianji.remark.domain.dto.LikeRecordFormDTO;
 import com.tianji.remark.domain.po.LikedRecord;
 import com.tianji.remark.mapper.LikedRecordMapper;
 import com.tianji.remark.service.ILikedRecordService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.tianji.common.constants.MqConstants.Exchange.LIKE_RECORD_EXCHANGE;
+import static com.tianji.common.constants.MqConstants.Key.LIKED_TIMES_KEY_TEMPLATE;
 
 /**
  * <p>
@@ -16,10 +25,52 @@ import org.springframework.stereotype.Service;
  * @since 2024-08-16
  */
 @Service
+@RequiredArgsConstructor
 public class LikedRecordServiceImpl extends ServiceImpl<LikedRecordMapper, LikedRecord> implements ILikedRecordService {
+
+    private final RabbitMqHelper mqHelper;
 
     @Override
     public void addLikeRecord(LikeRecordFormDTO recordDTO) {
+        // 判断点赞or取消点赞
+        boolean success = recordDTO.getLiked() ? like(recordDTO) : unlike(recordDTO);
+        if (!success) {
+            return;
+        }
+        // 统计点赞总数
+        Integer likedTimes = lambdaQuery()
+                .eq(LikedRecord::getBizId, recordDTO.getBizId())
+                .count();
+        // 发送mq通知
+        mqHelper.send(
+                LIKE_RECORD_EXCHANGE,
+                StringUtils.format(LIKED_TIMES_KEY_TEMPLATE, recordDTO.getBizType()),
+                LikedTimesDTO.of(recordDTO.getBizId(), likedTimes));
+    }
 
+    private boolean unlike(LikeRecordFormDTO recordDTO) {
+        return remove(new QueryWrapper<LikedRecord>().lambda()
+                .eq(LikedRecord::getUserId, UserContext.getUser())
+                .eq(LikedRecord::getBizId, recordDTO.getBizId()));
+    }
+
+    private boolean like(LikeRecordFormDTO recordDTO) {
+        Long userId = UserContext.getUser();
+        // 查询是否存在记录
+        Integer count = lambdaQuery()
+                .eq(LikedRecord::getUserId, userId)
+                .eq(LikedRecord::getBizId, recordDTO.getBizId())
+                .count();
+        // 存在则直接结束
+        if (count > 0) {
+            return false;
+        }
+        // 不存在则新增
+        LikedRecord r = new LikedRecord();
+        r.setBizId(recordDTO.getBizId());
+        r.setUserId(userId);
+        r.setBizType(recordDTO.getBizType());
+        save(r);
+        return true;
     }
 }
