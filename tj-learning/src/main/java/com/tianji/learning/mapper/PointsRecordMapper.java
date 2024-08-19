58a8d87fe71e6641e15ca153ec53d295f2cc47a9
Colin Wang <2802235800@qq.com>
2024-08-19 10:59:53 +0800

    积分系统-init-代码生成

---
+++ tj-learning/src/main/java/com/tianji/learning/mapper/PointsRecordMapper.java
@@ -0,0 +1,16 @@
+package com.tianji.learning.mapper;
+
+import com.tianji.learning.domain.po.PointsRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 学习积分记录，每个月底清零 Mapper 接口
+ * </p>
+ *
+ * @author colinwang
+ * @since 2024-08-19
+ */
+public interface PointsRecordMapper extends BaseMapper<PointsRecord> {
+
+}
