d7a2f3813452f1018ec0623260ce168e0738b225
Colin Wang <2802235800@qq.com>
2024-08-19 16:31:28 +0800

    积分系统-实现签到功能

---
+++ tj-learning/src/main/java/com/tianji/learning/service/impl/SignRecordServiceImpl.java
@@ -0,0 +1,78 @@
+package com.tianji.learning.service.impl;
+
+import com.tianji.common.exceptions.BizIllegalException;
+import com.tianji.common.utils.BooleanUtils;
+import com.tianji.common.utils.CollUtils;
+import com.tianji.common.utils.DateUtils;
+import com.tianji.common.utils.UserContext;
+import com.tianji.learning.constants.RedisConstants;
+import com.tianji.learning.domain.vo.SignResultVO;
+import com.tianji.learning.service.ISignRecordService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.connection.BitFieldSubCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SignRecordServiceImpl implements ISignRecordService {
+
+    private final StringRedisTemplate redisTemplate;
+
+    @Override
+    public SignResultVO addSignRecords() {
+        // 签到
+        Long userId = UserContext.getUser();
+        LocalDate now = LocalDate.now();
+        // -拼接key
+        String key = RedisConstants.SIGN_RECORD_KEY_PREFIX
+                + userId
+                + now.format(DateUtils.SIGN_DATE_SUFFIX_FORMATTER);
+        int offset = now.getDayOfMonth() - 1;
+        // -保存签到信息
+        Boolean exists = redisTemplate.opsForValue().setBit(key, offset, true);
+        if (BooleanUtils.isTrue(exists)) {
+            throw new BizIllegalException("不允许重复签到！");
+        }
+        int signDays = countSignDays(key, now.getDayOfMonth());
+        int rewardPoints = 0;
+        switch (signDays) {
+            case 7:
+                rewardPoints = 10;
+                break;
+            case 14:
+                rewardPoints = 20;
+                break;
+            case 28:
+                rewardPoints = 40;
+                break;
+        }
+        // TODO 保存积分明细记录
+
+        // 封装vo并返回
+        SignResultVO vo = new SignResultVO();
+        vo.setSignDays(signDays);
+        vo.setRewardPoints(rewardPoints);
+        return vo;
+    }
+
+    private int countSignDays(String key, int len) {
+        // 获取本月所有签到记录
+        List<Long> result = redisTemplate.opsForValue()
+                .bitField(key, BitFieldSubCommands.create().get(
+                        BitFieldSubCommands.BitFieldType.unsigned(len)).valueAt(0));
+        if (CollUtils.isEmpty(result)) {
+            return 0;
+        }
+        int num = result.get(0).intValue();
+        int count = 0;
+        while ((num & 1) == 1) {
+            count++;
+            num >>>= 1;
+        }
+        return count;
+    }
+}
