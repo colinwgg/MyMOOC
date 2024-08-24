b5debc6f7ac738e02d5744dcddbefc8e974be6df
Colin Wang <2802235800@qq.com>
2024-08-24 23:15:15 +0800

    优惠券管理-init

---
+++ tj-promotion/src/main/java/com/tianji/promotion/controller/ExchangeCodeController.java
@@ -0,0 +1,20 @@
+package com.tianji.promotion.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 兑换码 前端控制器
+ * </p>
+ *
+ * @author colinwang
+ * @since 2024-08-24
+ */
+@RestController
+@RequestMapping("/exchange-code")
+public class ExchangeCodeController {
+
+}
---
+++ tj-promotion/src/main/java/com/tianji/promotion/service/IExchangeCodeService.java
@@ -0,0 +1,16 @@
+package com.tianji.promotion.service;
+
+import com.tianji.promotion.domain.po.ExchangeCode;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 兑换码 服务类
+ * </p>
+ *
+ * @author colinwang
+ * @since 2024-08-24
+ */
+public interface IExchangeCodeService extends IService<ExchangeCode> {
+
+}
