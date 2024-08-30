1e41aa9ffbb73e1539ee487cb12254c775930a46
Colin Wang <2802235800@qq.com>
2024-08-30 19:15:30 +0800

    优惠券管理-领取优惠券

--- tj-promotion/src/main/java/com/tianji/promotion/service/impl/UserCouponServiceImpl.java
+++ tj-promotion/src/main/java/com/tianji/promotion/service/impl/UserCouponServiceImpl.java
@@ -1,10 +1,18 @@
 package com.tianji.promotion.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.tianji.common.exceptions.BadRequestException;
+import com.tianji.common.utils.UserContext;
+import com.tianji.promotion.domain.po.Coupon;
 import com.tianji.promotion.domain.po.UserCoupon;
+import com.tianji.promotion.mapper.CouponMapper;
 import com.tianji.promotion.mapper.UserCouponMapper;
 import com.tianji.promotion.service.IUserCouponService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -15,6 +23,53 @@ import org.springframework.stereotype.Service;
  * @since 2024-08-30
  */
 @Service
+@RequiredArgsConstructor
 public class UserCouponServiceImpl extends ServiceImpl<UserCouponMapper, UserCoupon> implements IUserCouponService {
 
+    private final CouponMapper couponMapper;
+
+    @Override
+    @Transactional
+    public void receiveCoupon(Long couponId) {
+        // 校验优惠券是否存在，不存在无法领取
+        Coupon coupon = couponMapper.selectById(couponId);
+        if (coupon == null) { throw new BadRequestException("优惠券不存在"); }
+        // 校验优惠券的发放时间，是不是正在发放中
+        LocalDateTime now = LocalDateTime.now();
+        if (now.isBefore(coupon.getIssueBeginTime()) || now.isAfter(coupon.getIssueEndTime())) {
+            throw new BadRequestException("优惠券发放已经结束或尚未开始");
+        }
+        // 校验优惠券剩余库存是否充足
+        if (coupon.getIssueNum() >= coupon.getTotalNum()) {
+            throw new BadRequestException("优惠券库存不足");
+        }
+        // 校验优惠券的每人限领数量
+        Long userId = UserContext.getUser();
+        Integer count = lambdaQuery()
+                .eq(UserCoupon::getUserId, userId)
+                .eq(UserCoupon::getCouponId, coupon.getId())
+                .count();
+        if (count != null && count >= coupon.getUserLimit()) {
+            throw new BadRequestException("超出领取数量");
+        }
+        // 更新优惠券的已经发放的数量 + 1
+        couponMapper.incrIssueNum(coupon.getId());
+        // 新增一个用户券
+        saveUserCoupon(coupon, userId);
+    }
+
+    private void saveUserCoupon(Coupon coupon, Long userId) {
+        UserCoupon userCoupon = new UserCoupon();
+        userCoupon.setUserId(userId);
+        userCoupon.setCouponId(coupon.getId());
+        LocalDateTime termBeginTime = coupon.getTermBeginTime();
+        LocalDateTime termEndTime = coupon.getTermEndTime();
+        if (termBeginTime == null) {
+            termBeginTime = LocalDateTime.now();
+            termEndTime = termBeginTime.plusDays(coupon.getTermDays());
+        }
+        userCoupon.setTermBeginTime(termBeginTime);
+        userCoupon.setTermEndTime(termEndTime);
+        save(userCoupon);
+    }
 }
