2fda042d43e0b488763afe1af0f8ff78e6164c3c
Colin Wang <2802235800@qq.com>
2024-09-05 22:23:24 +0800

    优惠券使用-细筛 查询优惠券的使用范围

--- tj-promotion/src/main/java/com/tianji/promotion/service/impl/IDiscountServiceImpl.java
+++ tj-promotion/src/main/java/com/tianji/promotion/service/impl/IDiscountServiceImpl.java
@@ -1,22 +1,31 @@
 package com.tianji.promotion.service.impl;
 
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
 import com.tianji.common.utils.CollUtils;
 import com.tianji.common.utils.UserContext;
 import com.tianji.promotion.domain.dto.CouponDiscountDTO;
 import com.tianji.promotion.domain.dto.OrderCourseDTO;
 import com.tianji.promotion.domain.po.Coupon;
+import com.tianji.promotion.domain.po.CouponScope;
+import com.tianji.promotion.enums.DiscountType;
 import com.tianji.promotion.mapper.UserCouponMapper;
+import com.tianji.promotion.service.ICouponScopeService;
 import com.tianji.promotion.service.IDiscountService;
+import com.tianji.promotion.strategy.discount.Discount;
 import com.tianji.promotion.strategy.discount.DiscountStrategy;
 import lombok.RequiredArgsConstructor;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class IDiscountServiceImpl implements IDiscountService {
 
     private final UserCouponMapper userCouponMapper;
+    private final ICouponScopeService scopeService;
 
     @Override
     public List<CouponDiscountDTO> findDiscountSolution(List<OrderCourseDTO> orderCourses) {
@@ -35,5 +44,33 @@ public class IDiscountServiceImpl implements IDiscountService {
         if (CollUtils.isEmpty(availableCoupons)) {
             return CollUtils.emptyList();
         }
+        // 细筛
+        Map<Coupon, List<OrderCourseDTO>> availableCouponMap = findAvailableCoupon(availableCoupons, orderCourses);
+    }
+
+    private Map<Coupon, List<OrderCourseDTO>> findAvailableCoupon(List<Coupon> coupons, List<OrderCourseDTO> courses) {
+        Map<Coupon, List<OrderCourseDTO>> map = new HashMap<>(coupons.size());
+        for (Coupon coupon : coupons) {
+            List<OrderCourseDTO> availableCoupons = courses;
+            if (coupon.getSpecific()) {
+                // 查询适用范围
+                List<CouponScope> scopes = scopeService.lambdaQuery().eq(CouponScope::getCouponId, coupon.getId()).list();
+                // 获取分类id
+                Set<Long> cateIds = scopes.stream().map(CouponScope::getBizId).collect(Collectors.toSet());
+                // 过滤课程表
+                availableCoupons = courses.stream().filter(c -> cateIds.contains(c.getCateId())).collect(Collectors.toList());
+            }
+            if (CollUtils.isEmpty(availableCoupons)) {
+                continue;
+            }
+            // 计算totalAmount
+            int totalAmount = availableCoupons.stream().mapToInt(OrderCourseDTO::getPrice).sum();
+            // 判断是否可用
+            Discount discount = DiscountStrategy.getDiscount(coupon.getDiscountType());
+            if (discount.canUse(totalAmount, coupon)) {
+                map.put(coupon, availableCoupons);
+            }
+        }
+        return map;
     }
 }
